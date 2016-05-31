50f308ef7d54065471d3d7f58ce9373dc8b00d09
Alexandre <alexandregomes@outlook.com>
2016-05-31 00:37:07 -0300

    Atualização de arquivos
    Arquivos atualizados: .gitignore e config.properties.

--- src/war/Main.java
+++ src/war/Main.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-            int width = 800;
-            int height = 600;
+        int width = 800;
+        int height = 600;
 
-            Game game = new Game(width, height);
-            
+        Game game = new Game(width, height);
+
         try {
             game.run();
         } catch (IOException ex) {
