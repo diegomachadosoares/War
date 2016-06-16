5865b870163419e4b42b0cd8810da738eefd3ed8
Diego M. Soares <diegomachadosoares@gmail.com>
2016-06-16 12:01:26 -0300

    attributes and some gets & sets

--- src/war/Jogador.java
+++ src/war/Jogador.java
@@ -1,5 +1,7 @@
 package war;
 
+import java.util.List;
+
 /**
  *
  * @author diegomachado
@@ -7,11 +9,32 @@ package war;
 public class Jogador {
     
     private String name;
-    private String cor;
+    private String color;
+    private List<Territorio> Territorios;
+    private Objetivo obj;
+    private List<Carta> cartas;
     
     
-    public Jogador(){
-        
+    public Jogador(String name, String color){
+        this.name = name;
+        this.color = color;
     }
     
+    
+    // Gets and Sets
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getColor(){
+        return this.color;
+    }
+    
+    public void setColor(String color){
+        this.color = color;
+    }
 }
