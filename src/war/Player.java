fcae945ff1b7a4c5bdeeefe4d621011c168818a8
Diego M. Soares <diegomachadosoares@gmail.com>
2016-06-17 01:31:18 -0300

    Added distributeTroops method

--- src/war/Player.java
+++ src/war/Player.java
@@ -7,34 +7,45 @@ import java.util.List;
  * @author diegomachado
  */
 public class Player {
-    
+
     private String name;
     private String color;
-    private List<Territory> Territorios;
-    private Objective obj;
-    private List<Card> cartas;
-    
-    
-    public Player(String name, String color){
+    private List<Territory> Territories;
+    private String objective;
+    private List<Card> cards;
+
+    public Player(String name, String color) {
         this.name = name;
         this.color = color;
     }
-    
-    
+
     // Gets and Sets
     public String getName() {
         return this.name;
     }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    
-    public String getColor(){
+
+    public String getColor() {
         return this.color;
     }
-    
-    public void setColor(String color){
+
+    public void setColor(String color) {
         this.color = color;
+    }
+
+    public String getObjective() {
+        return this.objective;
+    }
+
+    public void chooseObjective() {
+        Objective obj = Objective.getInstance();
+        this.objective = obj.getObjective();
+    }
+    
+    public void distributeTroops(Territory t, int n){
+        t.addTroops(n);
     }
 }
