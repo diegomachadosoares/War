5638eb6a768cf0e8f993d8d7422cb3cf69b483d5
Alexandre <alexandregomes@outlook.com>
2016-06-18 10:17:54 -0300

    Removed List<Territory> neighbors from Territory constructor. Added Map class (for  continents and territories).

--- src/war/Territory.java
+++ src/war/Territory.java
@@ -11,9 +11,9 @@ class Territory {
     private List<Territory> neighbors;
     private Player owner;
     private int numberOfTerritories;
-    public Territory(String name, List<Territory> neighbors, Player owner, int numberOfTerritories){
+    public Territory(String name, Player owner, int numberOfTerritories){
         this.name = name;
-        this.neighbors = neighbors;
+        //this.neighbors = neighbors;
         this.owner = owner;
         this.numberOfTerritories = numberOfTerritories;
     }
