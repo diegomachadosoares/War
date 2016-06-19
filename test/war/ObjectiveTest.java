d0341a12b18efc5c05cfb6d374fb75a66cee8264
Alexandre <alexandregomes@outlook.com>
2016-06-19 16:41:44 -0300

    Added test to verify if all player get a objective.

--- test/war/ObjectiveTest.java
+++ test/war/ObjectiveTest.java
@@ -1,5 +1,7 @@
 package war;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -15,7 +17,7 @@ public class ObjectiveTest {
             Player[] players = new Player[numPlayers]; //container for the player list
             for (Integer i = 0; i < numPlayers; i++) {
                 //Create numPlayers players
-                players[i] = new Player("joao" + i.toString(), "Azul" + i.toString());
+                players[i] = new Player("joao" + i.toString(), "Azul" + i.toString(), i);
             }
             //Each player must have an objective!
             for (Player player : players) {
@@ -25,8 +27,29 @@ public class ObjectiveTest {
             for (Player player : players) {
                 System.out.println(player.getObjective());
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
-    } 
+    }
+
+    @Test
+    public void testIfAllPlayersHaveAObjective() {
+        int numPlayers = 6; //Number of players
+        Player[] players = new Player[numPlayers]; //container for the player list
+        for (Integer i = 0; i < numPlayers; i++) {
+            //Create numPlayers players
+            players[i] = new Player("joao" + i.toString(), "Azul" + i.toString(), i);
+        }
+        //Each player must have an objective!
+        for (Player player : players) {
+            player.chooseObjective();
+        }
+
+        for (Player player : players) {
+            if (player.getObjective() == null) {
+                fail("Player "+player.getId()+" without objective.");
+            }
+        }
+        assertEquals(true, true);
+    }
 }
