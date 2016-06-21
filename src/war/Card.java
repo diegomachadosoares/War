f9a0422151b97dabadc95207d87a2350968bd76d
Alexandre <alexandregomes@outlook.com>
2016-06-20 23:18:09 -0300

    Added the attributes and getters and setters to Card class.

--- src/war/Card.java
+++ src/war/Card.java
@@ -5,5 +5,25 @@ package war;
  * @author diegomachado
  */
 class Card {
+    private Territory territory;
+    private String figure;
+
+    public Card(Territory territory, String figure){
+        this.territory = territory;
+        this.figure = figure;
+    }
     
+    /**
+     * @return the territory
+     */
+    public Territory getTerritory() {
+        return territory;
+    }
+
+    /**
+     * @return the figure
+     */
+    public String getFigure() {
+        return figure;
+    }
 }
