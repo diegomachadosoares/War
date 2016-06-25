2270cecb1818ed2075b9c65579ed46f89cc80dc6
Alexandre <alexandregomes@outlook.com>
2016-06-25 17:44:28 -0300

    Created the methods distributeTerritories and distributeObjectives in Match class.

--- src/war/Match.java
+++ src/war/Match.java
@@ -5,8 +5,12 @@
  */
 package war;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,13 +20,64 @@ public class Match {
 
     private int numberOfExchanges = 0;
     private int numberOfTroops = 2;
+    List<Player> players;
+    Board board;
 
-    public void startMatch(List<Player> players) {
+    public Match(){
+        
+    }
+    
+    public Match(List<Player> players, Board board) {
+        this.players = players;
+        this.board = board;
+    }
+    
+    public void distributeTerritories() {
+        //Distribuir territorios
+        int territoriesCount = 0;
+        for (int i = 0; i < board.continents.size(); i++) {
+            for (int j = 0; j < board.continents.get(i).getTerritories().size(); j++) {
+                if (territoriesCount == players.size()) {
+                    territoriesCount = territoriesCount % players.size();
+                }
+                Territory t = board.continents.get(i).getTerritories().get(j);
+                Player p = players.get(territoriesCount);
+
+                t.setOwner(p);
+                p.addTerritory(t);
+                t.setOwner(p);
+
+                players.set(territoriesCount, p);
+                board.continents.get(i).getTerritories().set(j, t);
+
+                territoriesCount++;
+            }
+        }
+    }
+    
+    public void distributeObjectives(){
+        //Distribuir objetivos
+        for (int i = 0; i < players.size(); i++) {
+            players.get(i).chooseObjective();
+        }
+    }
+
+    public void startMatch() {
         boolean acabou = true;
+
+        //Inicializacao
+        
+
+        //Rodada de fortificação
+        for (int i = 0; i < players.size(); i++) {
+
+        }
 
         while (!acabou) {
             for (int i = 0; i < players.size(); i++) {
-
+                //Distribui peças
+                //Ataca / Combate
+                //Movimenta tropas
             }
         }
     }
@@ -30,9 +85,9 @@ public class Match {
     public int exchangeCards(Card card1, Card card2, Card card3) {
         if ((card1.getFigure().equalsIgnoreCase(card2.getFigure()) && card2.getFigure().equalsIgnoreCase(card3.getFigure()))
                 || !card1.getFigure().equalsIgnoreCase(card2.getFigure()) && !card1.getFigure().equalsIgnoreCase(card3.getFigure()) && !card2.getFigure().equalsIgnoreCase(card3.getFigure())) {
-                numberOfTroops += 2;
-                numberOfExchanges++;
-                return numberOfTroops;
+            numberOfTroops += 2;
+            numberOfExchanges++;
+            return numberOfTroops;
         }
         return 0;
     }
