8c459176823e097a3dd36cd9820808f5572e21d3
Alexandre <alexandregomes@outlook.com>
2016-06-25 17:46:21 -0300

    Created tests to distributeTerritories and distributeObjectives methods.

--- test/war/MatchTest.java
+++ test/war/MatchTest.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package war;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,26 +20,25 @@ import static org.junit.Assert.*;
  * @author AleGomes
  */
 public class MatchTest {
-    
+
     public MatchTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
 
     /**
      * Test of exchangeCards method, of class Match.
@@ -46,62 +46,120 @@ public class MatchTest {
     @Test
     public void testExchangeEqualCards() {
         //Test if all exchange when all cards are equal
-        
+
         Card c1 = new Card(new Territory("Territory1", null, 0), "Square");
         Card c2 = new Card(new Territory("Territory2", null, 0), "Square");
         Card c3 = new Card(new Territory("Territory3", null, 0), "Square");
-        
+
         Match m = new Match();
-        
+
         int numberOfTroops = m.exchangeCards(c1, c2, c3);
-        
+
         assertEquals(4, numberOfTroops);
-        
+
     }
-    
+
     @Test
     public void testExchangeDifferentCards() {
         Card c1 = new Card(new Territory("Territory1", null, 0), "Square");
         Card c2 = new Card(new Territory("Territory2", null, 0), "Triangle");
         Card c3 = new Card(new Territory("Territory3", null, 0), "Circle");
-        
+
         Match m = new Match();
-        
+
         int numberOfTroops = m.exchangeCards(c1, c2, c3);
-        
+
         assertEquals(4, numberOfTroops);
     }
-    
+
     @Test
     public void testExchangeIncompleteSetCards() {
         Card c1 = new Card(new Territory("Territory1", null, 0), "Square");
         Card c2 = new Card(new Territory("Territory2", null, 0), "Triangle");
         Card c3 = new Card(new Territory("Territory3", null, 0), "Square");
-        
+
         Match m = new Match();
-        
+
         int numberOfTroops = m.exchangeCards(c1, c2, c3);
-        
-        
+
         assertEquals(0, numberOfTroops);
     }
-    
+
     @Test
     public void testSerieOfExchanges() {
         Card c1 = new Card(new Territory("Territory1", null, 0), "Square");
         Card c2 = new Card(new Territory("Territory2", null, 0), "Triangle");
         Card c3 = new Card(new Territory("Territory3", null, 0), "Circle");
-        
+
         Match m = new Match();
-        
-        int[] expectedNumberOfTroops = {4,6,8};
-        
+
+        int[] expectedNumberOfTroops = {4, 6, 8};
+
         int[] numberOfTroops = new int[3];
         numberOfTroops[0] = m.exchangeCards(c1, c2, c3);
         numberOfTroops[1] = m.exchangeCards(c1, c2, c3);
         numberOfTroops[2] = m.exchangeCards(c1, c2, c3);
-        
+
         assertArrayEquals(expectedNumberOfTroops, numberOfTroops);
     }
-}
 
+    @Test
+    public void testDistributeObjectives() throws IOException {
+
+        Player p1 = new Player("p1", "Blue", 0);
+        Player p2 = new Player("p2", "Blue", 1);
+        Player p3 = new Player("p3", "Blue", 2);
+
+        List<Player> players = new ArrayList<>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+
+        Board board = new Board("territorios.txt", "vizinhos.txt");
+
+        Match match = new Match(players, board);
+
+        match.distributeObjectives();
+
+        for (Player p : match.players) {
+            if (p.getObjective() == null) {
+                fail("Player without objective.");
+            }
+        }
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void testDistributeTerritories() throws IOException {
+        Player p1 = new Player("p1", "Blue", 0);
+        Player p2 = new Player("p2", "Blue", 1);
+        Player p3 = new Player("p3", "Blue", 2);
+
+        List<Player> players = new ArrayList<>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+
+        Board board = new Board("territorios.txt", "vizinhos.txt");
+
+        Match match = new Match(players, board);
+        
+        match.distributeTerritories();
+        
+        for(Player p : match.players){
+            if(p.getTerritories() == null){
+                fail("No territories for player "+p.getName());
+            }
+        }
+        
+        for(Continent c : match.board.continents){
+            for(Territory t : c.getTerritories()){
+                if(t.getOwner() == null){
+                    fail("Territory "+t.getName()+" doenst have owner.");
+                }
+            }
+        }
+        
+        assertEquals(true, true);
+    }
+}
