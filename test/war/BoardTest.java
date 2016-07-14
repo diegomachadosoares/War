095f1f7b253ce4891d4e8c6c2ba58b7f4e7b1b29
Diego M. Soares <diegomachadosoares@gmail.com>
2016-07-13 21:19:19 -0300

    added testContinentTerritoryCreation

--- test/war/BoardTest.java
+++ test/war/BoardTest.java
@@ -1,14 +1,10 @@
 package war;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -45,33 +41,6 @@ public class BoardTest {
     @After
     public void tearDown() {
     }
-//
-//    @Test
-//    public void testIfCreatedAllContinents(){
-//        try {
-//            Board board = new Board("territorios.txt", "vizinhos.txt");
-//            
-//            assertEquals(board.continents.size(), 6);
-//        } catch (IOException ex) {
-//            Logger.getLogger(BoardTest.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//    
-//    @Test
-//    public void testIfCreatedAllTerritories() {
-//        try {
-//            Board board = new Board("territorios.txt", "vizinhos.txt");
-//
-//            int numberOfTerritories = 0;
-//            
-//            for(Continent c : board.continents){
-//                numberOfTerritories += c.getTerritories().size();
-//            }
-//            assertEquals(numberOfTerritories, 42);
-//        } catch (IOException ex) {
-//            Logger.getLogger(BoardTest.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
 
     @Test
     public void testTerritoriesCreation() throws IOException {
@@ -81,7 +50,7 @@ public class BoardTest {
         Territory t;
         while (!territories.isEmpty()) {
             t = (Territory) territories.get(i);
-            System.out.println("\t" +t.getID() + " " + t.getName());
+            System.out.println("\t" + t.getID() + " " + t.getName());
             territories.remove(i);
             i++;
         }
@@ -92,37 +61,21 @@ public class BoardTest {
     public void testContinentsCreation() throws IOException {
         System.out.println("\n$--- Beginning Continent creation tests... ---$\n");
         Map continentMap = this.board.getContinents();
-        int i = 0;
         Continent cont;
         List<Territory> t;
-        while (!continentMap.isEmpty()) {
-            cont = (Continent) continentMap.get(i);
-            System.out.println(cont.getName());
-            t = cont.getTerritories();
-            continentMap.remove(i);
-            i++;
+        for (int j = 0; j < continentMap.size() - 1; j++) {
+            cont = (Continent) continentMap.get(j);
+            System.out.println("\nCONTINENTE: " + cont.getName() + "\n");
+            testContinentTerritoryCreation(cont);
         }
         System.out.println("\n$--- End of Continent creation tests... ---$\n");
     }
-    
-//    @Test
-//    public void testContinentTerritoryCreation(){
-//        Map contMap = board.getContinents();
-//        Continent c;
-//        List<Territory> t;
-//        int i = 0;
-//        int j = 0;
-//        while(!contMap.isEmpty()){
-//            c = (Continent)contMap.get(i);
-//            t = c.getTerritories();
-//            while(!t.isEmpty()){
-//                System.out.println(t.get(j));
-//                t.remove(j);
-//                j++;
-//            }
-//            System.out.println(c.getTerritories());
-//            contMap.remove(i);
-//            i++;
-//        }
-//    }
+
+    public void testContinentTerritoryCreation(Continent c) {
+        List<Territory> t;
+        t = c.getTerritories();
+        for (int j = 0; j < t.size() - 1; j++) {
+            System.out.println(t.get(j).getName());
+        }
+    }
 }
