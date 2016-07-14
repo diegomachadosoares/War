5a71fc10df3a9c42dd54bbf8df997e1693c9ed17
Diego M. Soares <diegomachadosoares@gmail.com>
2016-07-13 21:18:29 -0300

    Fixed arraylist initialization

--- src/war/Board.java
+++ src/war/Board.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -18,10 +18,12 @@ public class Board {
 
     private final Map<Integer, Continent> continents;
     private final Map<Integer, Territory> territories;
+    private final Map<Integer, List<Territory>> neighborhood;
 
     public Board(String continentsFile, String territoriesFile, String neighborhoodFile) throws FileNotFoundException, IOException {
         this.continents = new HashMap<>();
         this.territories = new HashMap<>();
+        this.neighborhood = new HashMap<>();
         readTerritories(territoriesFile);
         readContinents(continentsFile);
         //readNeighborhood(neighborhoodFile);
@@ -46,13 +48,12 @@ public class Board {
             Scanner sc = new Scanner(new FileInputStream(new File(continentsFile)));            
             String currline;
             String[] split, t;
-            List<Territory> tlist = new LinkedList();
+            List<Territory> tlist = new ArrayList();
             while(sc.hasNextLine()){
                 currline = sc.nextLine();
                 split = currline.split(":");
-                t = split[2].split(";");
-                for (String t1 : t) {
-                    tlist.add(getTerritory(Integer.parseInt(t1)));
+                for (int i = 2; i < split.length - 1; i++) {
+                    tlist.add(getTerritory(Integer.parseInt(split[i])));
                 }
                 Continent c = new Continent(split[1], Integer.parseInt(split[0]), tlist);
                 this.continents.put(Integer.parseInt(split[0]), c);
@@ -61,6 +62,17 @@ public class Board {
         }
     }
 
+    private void readNeighborhood(String neighborhoodFile) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileInputStream(new File(neighborhoodFile)));
+        String currline;
+        String[] split;
+        List<Territory> tlist = new ArrayList();
+        while(sc.hasNextLine()){
+            currline = sc.nextLine();
+            split = currline.split(":");
+        }
+    }
+    
     public Map getTerritories(){
         return this.territories;
     }
