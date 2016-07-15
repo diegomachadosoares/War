1380b7246ec2b46ed3f64139a72bc62d0166af49
Diego M. Soares <diegomachadosoares@gmail.com>
2016-07-14 22:29:33 -0300

    Major rewrite

--- src/war/Objective.java
+++ src/war/Objective.java
@@ -1,7 +1,6 @@
 package war;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 /**
  *
@@ -9,54 +8,29 @@ import java.util.Map;
  */
 public class Objective {
 
-    private static Objective instance;
-    private static final Map<Integer, String> objectives;
-
-    static {
-        objectives = new HashMap<>();
-
-        objectives.put(1, "Conquistar na totalidade a EUROPA, a OCEANIA e mais um terceiro.");
-        objectives.put(2, "Conquistar na totalidade a ASIA e a AMÉRICA DO SUL.");
-        objectives.put(3, "Conquistar na totalidade a EUROPA, a AMÉRICA DO SUL e mais um terceiro.");
-        objectives.put(4, "Conquistar 18 TERRITÓRIOS e ocupar cada um deles com pelo menos dois exércitos.");
-        objectives.put(5, "Conquistar na totalidade a ASIA e a ÁFRICA.");
-        objectives.put(6, "Conquistar na totalidade a AMÉRICA DO NORTE e a ÁFRICA.");
-        objectives.put(7, "Conquistar 24 TERRITÓRIOS à sua escolha.");
-        objectives.put(8, "Conquistar na totalidade a AMÉRICA DO NORTE e a OCEANIA.");
-        objectives.put(9, "Destruir totalmente OS EXÉRCITOS AZUIS ou conquistar 18 territórios caso ele não esteja em jogo.");
-        objectives.put(10, "Destruir totalmente OS EXÉRCITOS AMARELOS ou conquistar 18 territórios caso ele não esteja em jogo.");
-        objectives.put(11, "Destruir totalmente OS EXÉRCITOS VERMELHOS ou conquistar 18 territórios caso ele não esteja em jogo.");
-        objectives.put(12, "Destruir totalmente OS EXÉRCITOS PRETOS ou conquistar 18 territórios caso ele não esteja em jogo.");
-        objectives.put(13, "Destruir totalmente OS EXÉRCITOS BRANCOS ou conquistar 18 territórios caso ele não esteja em jogo.");
-        objectives.put(14, "Destruir totalmente OS EXÉRCITOS VERDES ou conquistar 18 territórios caso ele não esteja em jogo.");
+    private int type;
+    private String msg;
+    private List<Integer> target;
+    private String color;
+    private int nTerritories;
+    
+    private Objective(int type, String msg){
+        this.type = type;
+        this.msg = msg;
+    }
+    
+    public Objective(int type, String msg, List<Integer> continents){
+        this(type, msg);
+        this.target = continents;
     }
 
-    public static String getObjective() {
-        String o = null;
-        int pos;
-        while (true) {
-            pos = (int) (1 + Math.random() * 14);
-            if ((objectives.get(pos) != null) ){
-                o = objectives.get(pos);
-                objectives.remove(pos);
-                break;
-            } else if (objectives.isEmpty()){
-                break;
-            }
-        }
-        return o;
+    public Objective(int type, String msg, String color){
+        this(type, msg);
+        this.color = color;
     }
-
-    public static Objective getInstance() {
-        if (instance == null) {
-            initializeInstance();
-        }
-        return instance;
-    }
-
-    private static synchronized void initializeInstance() {
-        if (instance == null) {
-            instance = new Objective();
-        }
+    
+    public Objective(int type, String msg, int nterritories){
+        this(type, msg);
+        this.nTerritories = nterritories;
     }
 }
