103931a97f900b856711ae625dd15c1ceed8d19e
thalesathayde <thales.athayde.santos@gmail.com>
2016-07-16 19:40:43 -0300

    soldados ja desenhados na tela

--- src/war/MapView.java
+++ src/war/MapView.java
@@ -8,6 +8,7 @@ package war;
 import JPlay.GameImage;
 import JPlay.Keyboard;
 import JPlay.Mouse;
+import JPlay.Sprite;
 import JPlay.Window;
 import java.awt.Color;
 import java.io.File;
@@ -28,6 +29,7 @@ public class MapView {
     private GameImage hud;
     //private Hud_Dialog menu_lateral = new Hud_Dialog();
     private GameImage shadow;
+    private Sprite[] soldier;
 
     List<Button> buttons = new ArrayList<Button>(); // Lista de botões
 
@@ -41,6 +43,7 @@ public class MapView {
         this.keyboard = window.getKeyboard();
         this.mouse = window.getMouse();
         backgroundDrawn = false;
+        soldier = new Sprite[42];
         
         try {
             File file = new File("data/pais_button.ini");
@@ -48,8 +51,10 @@ public class MapView {
 
             int max = reader.nextInt();
             for (int i = 0; i < max; i++) {
-            buttons.add(new Button(reader.next(), reader.nextInt(), reader.nextInt(), this.mouse));
-
+                buttons.add(new Button(reader.next(), reader.nextInt(), reader.nextInt(), this.mouse));
+                soldier[i] = new Sprite("data/gameplay/sprite soldados 20x20.png", 6);
+                soldier[i].setPosition(1000,1000);  //para os soldados nao serem printados 
+                                                    //na tela antes do jogo começar
             }
             
         } catch (FileNotFoundException ex) {
@@ -122,6 +127,10 @@ public class MapView {
 
 
         }
+        
+        for(int i=0; i<42; i++){
+            soldier[i].draw();
+        }
        
            
    
@@ -150,25 +159,32 @@ public class MapView {
                 String quantidade = Integer.toString(territory.getTroops());
                 int posX = reader.nextInt()+buttons.get(i).getSprite().width/2; 
                 int posY = (int)(reader.nextInt()+buttons.get(i).getSprite().height*1.5);
+                soldier[i].setPosition(posX-soldier[i].width, posY-buttons.get(i).getSprite().height*0.5);
                 
                 switch(player.getColor()){
                     case "Amarelo":
-                        window.drawText(quantidade, posX, posY, Color.YELLOW);
+                        window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(5);
                         break;
                     case "Azul":
-                        window.drawText(quantidade, posX, posY, Color.BLUE);
+                        window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(1);
                         break;
                     case "Roxo":
-                        window.drawText(quantidade, posX, posY, new java.awt.Color(198,5,246));
+                        window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(3);
                         break;
                     case "Preto":
                         window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(0);
                         break;
                     case "Verde":
-                        window.drawText(quantidade, posX, posY, Color.GREEN);
+                        window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(2);
                         break;
                     case "Vermelho":
-                        window.drawText(quantidade, posX, posY, Color.RED);
+                        window.drawText(quantidade, posX, posY, Color.BLACK);
+                        soldier[i].setCurrFrame(4);
                         break;
                     default:
                 }
