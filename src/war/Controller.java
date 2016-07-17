fe6670d808fc47d7853785f3d85018d3f9a740b8
Diego M. Soares <diegomachadosoares@gmail.com>
2016-07-17 13:58:29 -0300

    Fixed elegerAlvos method

--- src/war/Controller.java
+++ src/war/Controller.java
@@ -75,26 +75,18 @@ public class Controller {
     public Map getContinents() {
         return board.getContinents();
     }
-    
-    public List<Territory> elegerAlvos( int indice ) {
-    List<Territory> alvos = getNeighborhood(indice);
-    ArrayList<Integer> positions = new ArrayList();
-    
-        for (int i = 0; i < alvos.size(); i++) {
-            if(alvos.get(i).getOwner().getId()==0){
-            positions.add(i);
+
+    public List<Territory> elegerAlvos(int indice) {
+        List<Territory> neighbors = getNeighborhood(indice);
+        ArrayList<Territory> target = new ArrayList();
+        for (int i = 0; i < neighbors.size(); i++) {
+            if (neighbors.get(i).getOwner().getId() == getTerritory(indice).getID()) {
+                target.add(getTerritory(indice));
             }
-            
         }
-    
-        for (int i = 0; i < positions.size(); i++) {
-            Territory t = alvos.get(positions.get(i));
-            alvos.remove(t);                        
-        }
-        
-    return alvos;
+        return target;
     }
-    
+
     public Map getNeighborhoods() {
         return board.getNeighborhoods();
     }
@@ -204,9 +196,9 @@ public class Controller {
 
         }
     }
-    
-    public void setNTroops(int i , int j){
-    this.players.get(i).setNTroops(j);
+
+    public void setNTroops(int i, int j) {
+        this.players.get(i).setNTroops(j);
     }
 
     public List getPlayers() {
@@ -218,8 +210,8 @@ public class Controller {
     public static void clearStateForTesting() {
         INSTANCE = null;
     }
-    
-    public void addTroops(int id, int nTroops){
+
+    public void addTroops(int id, int nTroops) {
         board.getTerritory(id).addTroops(nTroops);
     }
 }
